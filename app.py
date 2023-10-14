05e1eccf368584e2b262cd7ad6378f237be1f915
ChathukaViduranga <chathuka.viduranga.bc@gmail.com>
2023-10-15 02:46:37 +0530

    cors added

--- app.py
+++ app.py
@@ -2,8 +2,11 @@ import joblib
 import numpy as np
 from flask import Flask, request, jsonify
 import sklearn
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+CORS(app)
 
 # Load the trained model
 model = joblib.load('StockPrediction_model.pkl')
